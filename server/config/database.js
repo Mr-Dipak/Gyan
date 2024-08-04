86fb748b41a93013707c04f522e86721de5eea22
mr-dipak <mr.dipaknrathod@gmail.com>
2024-08-05 02:36:31 +0530

    update

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,6 @@
-// config/database.js
-
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import mysql2 from 'mysql2'; // Import mysql2
 
 // Load environment variables from .env file
 dotenv.config();
@@ -9,6 +8,7 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
+  dialectModule: mysql2, // Explicitly provide the mysql2 module
   port: process.env.DB_PORT,
   logging: false, // Optional: Set to `console.log` if you want to see SQL queries in the console
 });
