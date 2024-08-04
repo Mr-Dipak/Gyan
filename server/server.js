ce10450e4e4298b656c1ddbb8ce1faf38bd1a8e4
mr-dipak <mr.dipaknrathod@gmail.com>
2024-08-05 03:09:13 +0530

    update

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ sequelize.sync().then(() => {
   console.error('Unable to sync database:', err);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
