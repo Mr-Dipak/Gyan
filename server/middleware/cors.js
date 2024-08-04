b3927e9d277640a8a256909dd0b13432c5330165
mr-dipak <mr.dipaknrathod@gmail.com>
2024-08-05 01:26:53 +0530

    update

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -2,10 +2,10 @@
 
 import cors from 'cors';
 
-
+// Set up CORS middleware
 const corsOptions = {
-  origin: true,
-  credentials: true
+  origin: true, 
+  credentials: true 
 };
 
 export default cors(corsOptions);
--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ sequelize.sync().then(() => {
   console.error('Unable to sync database:', err);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
